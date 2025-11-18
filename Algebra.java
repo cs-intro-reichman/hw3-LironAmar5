4fbc3250d29189ce6caf742be488d9883d19a1a4
Liron Amar <liron.amar@post.runi.ac.il>
2025-11-18 11:44:10 +0200

    last modify Algebra

--- Algebra.java
+++ Algebra.java
@@ -187,15 +187,26 @@ public class Algebra {
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		if(x == 0 ){return 0;}
-		else if (x < 0) { return -1;}
-		else{
-     int temp = 1;
-	 while (times(temp,temp) < x){
-		temp++;
-	 }
-	
-	return temp;
-    }
+		if ( x < 0){
+			throw new IllegalArgumentException("negative number");
+		}
+		if (( x == 0) || ( x == 1)){ return x;}
+
+		int low = 1;
+		int high = x;
+		int ans = 0;
+
+		while ( low <= high){
+			int mid = low + (high - low) / 2;
+
+			if( mid <= x / mid) {
+               ans = mid;
+			   low = mid + 1;
+			}
+			else{
+				high = mid - 1;
+			}
+		}
+        return ans;
     }
 }
