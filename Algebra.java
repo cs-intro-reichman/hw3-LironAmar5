dde0bc52393e9f296e0194371568671fa8839e93
Liron Amar <liron.amar@post.runi.ac.il>
2025-11-18 11:51:23 +0200

    Algebra and Loan Done

--- Algebra.java
+++ Algebra.java
@@ -197,14 +197,14 @@ public class Algebra {
 		int ans = 0;
 
 		while ( low <= high){
-			int mid = low + (high - low) / 2;
+			int mid = plus(low , div((minus(high ,low)) , 2));
 
-			if( mid <= x / mid) {
+			if( mid <= div(x , mid)) {
                ans = mid;
-			   low = mid + 1;
+			   low = plus(mid, 1);
 			}
 			else{
-				high = mid - 1;
+				high = minus(mid , 1);
 			}
 		}
         return ans;
